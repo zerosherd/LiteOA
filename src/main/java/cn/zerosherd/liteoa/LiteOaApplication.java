20e162272f8fc3f57a48a7a536d09b093302e428
zeroshed <ziloongtse@qq.com>
2024-10-01 23:15:58 +0800

    Base Code

---
+++ src/main/java/cn/zerosherd/liteoa/LiteOaApplication.java
@@ -0,0 +1,13 @@
+package cn.zerosherd.liteoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteOaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteOaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/zerosherd/liteoa/LiteOaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zerosherd.liteoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteOaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
